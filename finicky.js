d2a96ad93d8565b7e5394441aaf7624eb5161e88
Joshua Huh <joshua.huh@anglican.kr>
2020-03-15 10:28:18 +0900

    Update finicky.js
    Assign personal notion to Firefox, and company to Google Chrome

--- finicky.js
+++ finicky.js
@@ -2,10 +2,28 @@ module.exports = {
   defaultBrowser: "Firefox",
   handlers: [
     {
+      match: ({ sourceBundleIdentifier }) => {
+        finicky.notify(sourceBundleIdentifier);
+        finicky.log(sourceBundleIdentifier);
+      },
+    browser: "Firefox",
+    },
+    {
+      match: /https:\/\/www\.notion\.so\/joshuainboots/,
+      browser: "Firefox"
+    },
+    {
+      match: /https:\/\/www\.notion\.so\/logpresso/,
+      browser: "Google Chrome"
+    },
+    {
       // Open google.com and *.google.com urls in Google Chrome
       match: finicky.matchHostnames([
-        "google.com", // match google.com domain as string (to make regular expression less complicated)
-        /.*\.google.com$/, // match all google.com subdomains
+        // "google.com", // match google.com domain as string (to make regular expression less complicated)
+        "accounts.google.com",
+        "doc.google.com",
+        "drive.google.com",
+        ///.*\.google.com$/, // match all google.com subdomains
         /.*\.youtube.com$/, // match all youtube.com subdomains
         /.*\.local$/
       ]),

2e5a1ab6e9c74571b4a0f9e22e8f454b2f2086ca
Joshua Huh <joshua.huh@anglican.kr>
2020-04-01 13:32:54 +0900

    Open any G Suite files in Google File Stream with Google Chrome

--- finicky.js
+++ finicky.js
@@ -1,5 +1,6 @@
 module.exports = {
   defaultBrowser: "Firefox",
+
   handlers: [
     {
       match: ({ sourceBundleIdentifier }) => {
@@ -7,22 +8,39 @@ module.exports = {
       },
     browser: "Firefox",
     },
+
+    // Open my personal notion notes in Firefox
     {
       match: /.*\.notion\.so\/joshuainboots/,
       browser: "Firefox"
     },
+
+    // Open notion nontes of working place in Google Chrome 
     {
+      
       match: /.*\.notion\.so\/logpresso/,
       browser: "Google Chrome"
     },
+
+    // Open any anglican.kr links in Safari
     {
       match: finicky.matchHostnames([
         /.*\.anglican.kr$/
       ]),
       browser: "Safari"
     },
+
+    // Open any G Suite files in Google File Stream with Google Chrome
     {
-      // Open google.com and *.google.com urls in Google Chrome
+    
+      match: ({ sourceBundleIdentifier }) =>
+        ["com.google.drivefs"].includes(sourceBundleIdentifier),
+      browser: "Google Chrome"
+    },
+
+    // Open google.com and youtube.com urls in Google Chrome
+    {
+    
       match: finicky.matchHostnames([
         // "google.com", // match google.com domain as string (to make regular expression less complicated)
         "accounts.google.com",
